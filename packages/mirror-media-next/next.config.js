6dfd8575a10b9423e34e07c626079dcdc511fc50
DianYangFu <dianyangfu@gmail.com>
2022-10-31 16:22:26 +0800

    chore(mm-next): update styled-component related config

--- packages/mirror-media-next/next.config.js
+++ packages/mirror-media-next/next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  compiler: {
+    styledComponents: {
+      displayName: true,
+      ssr: true,
+    },
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
