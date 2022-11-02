9470a3767ab77836543a2a66b7217eaa9b9408c6
DianYangFu <dianyangfu@gmail.com>
2022-11-02 15:24:25 +0800

    chore(mm-next): add comment to explain the purpose of `_document.js`

--- packages/mirror-media-next/pages/_document.js
+++ packages/mirror-media-next/pages/_document.js
@@ -1,3 +1,7 @@
+//This file is used to override default Document of Nextjs, and update update the `<html>` and `<body>` tags used to render a page.
+//We also add server-side setting of `styled-components`, which can be rendered at timing of page has visible content but not interactive.
+//For more detail, please visited documentation of [Nextjs](https://nextjs.org/docs/advanced-features/custom-document) and [styled-component](https://styled-components.com/docs/advanced#server-side-rendering)
+
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
