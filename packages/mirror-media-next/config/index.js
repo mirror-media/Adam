eb7cb06613d6ed3c6420e713d71d2d06edadf83a
DianYangFu <dianyangfu@gmail.com>
2022-11-08 20:46:44 +0800

    chore(mm-next): add env `RESTFUL_API_HOST`

--- packages/mirror-media-next/config/index.js
+++ packages/mirror-media-next/config/index.js
@@ -5,8 +5,8 @@ const URL_MIRROR_MEDIA =
 
 const ENV = process.env.ENV || 'local'
 const API_PROTOCOL = process.env.API_PROTOCOL || 'http'
+const RESTFUL_API_HOST = process.env.RESTFUL_API_HOST || 'no-api-host'
 const API_PORT = process.env.API_PORT || '8080'
-const API_HOST = process.env.API_HOST || 'no-api-host'
 
 // The following variables are given values according to different `ENV`
 let API_TIMEOUT = 5000
@@ -18,25 +18,25 @@ switch (ENV) {
     API_TIMEOUT = 1500
     URL_STATIC_COMBO_SECTIONS =
       'https://storage.googleapis.com/statics.mirrormedia.mg/json/sections.json'
-    URL_STATIC_COMBO_TOPICS = `${API_PROTOCOL}://${API_HOST}:${API_PORT}/combo?endpoint=topics`
+    URL_STATIC_COMBO_TOPICS = `${API_PROTOCOL}://${RESTFUL_API_HOST}:${API_PORT}/combo?endpoint=topics`
   case 'staging':
     API_TIMEOUT = 1500
     URL_STATIC_COMBO_SECTIONS =
       'https://storage.googleapis.com/statics.mirrormedia.mg/json/sections.json'
-    URL_STATIC_COMBO_TOPICS = `${API_PROTOCOL}://${API_HOST}:${API_PORT}/combo?endpoint=topics`
+    URL_STATIC_COMBO_TOPICS = `${API_PROTOCOL}://${RESTFUL_API_HOST}:${API_PORT}/combo?endpoint=topics`
 
     break
   case 'dev':
     API_TIMEOUT = 5000
     URL_STATIC_COMBO_SECTIONS =
       'https://storage.googleapis.com/statics.mirrormedia.mg/json/sections.json'
-    URL_STATIC_COMBO_TOPICS = `${API_PROTOCOL}://${API_HOST}:${API_PORT}/combo?endpoint=topics`
+    URL_STATIC_COMBO_TOPICS = `${API_PROTOCOL}://${RESTFUL_API_HOST}:${API_PORT}/combo?endpoint=topics`
 
     break
   default:
     API_TIMEOUT = 5000
-    URL_STATIC_COMBO_SECTIONS = `${API_PROTOCOL}://${API_HOST}:${API_PORT}/json/sections.json`
-    URL_STATIC_COMBO_TOPICS = `${API_PROTOCOL}://${API_HOST}:${API_PORT}/api/v2/combo?endpoint=topics`
+    URL_STATIC_COMBO_SECTIONS = `${API_PROTOCOL}://${RESTFUL_API_HOST}:${API_PORT}/json/sections.json`
+    URL_STATIC_COMBO_TOPICS = `${API_PROTOCOL}://${RESTFUL_API_HOST}:${API_PORT}/api/v2/combo?endpoint=topics`
 }
 
 export {
