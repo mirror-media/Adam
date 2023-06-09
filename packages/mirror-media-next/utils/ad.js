893f3d9bcbdaa132faa7ba7925794b5d8c12a394
Xuan <as840922qq@gmail.com>
2023-06-09 12:17:06 +0800

    feat(ad): add `MicroAd` utils

---
+++ packages/mirror-media-next/utils/ad.js
@@ -0,0 +1,47 @@
+import { MICRO_AD_UNITS } from '../constants/ads'
+
+/**
+ * Determining whether to insert a `Micro` advertisement after a specific post index.
+ *
+ * @param {number} index
+ * @returns {boolean}
+ */
+const needInsertMicroAdAfter = (index) => {
+  if (typeof index !== 'number') {
+    return false
+  }
+
+  return index === 1 || index === 3 || index === 5
+}
+
+/**
+ * @typedef {'HOME' | 'LISTING' | 'STORY' } MicroAdType
+ */
+
+/**
+ * @typedef {'PC' | 'MB' | 'RWD' } Device
+ */
+
+/**
+ * Determining which Micro advertisement ID to take based on the `index`.
+ *
+ * @param {number} index
+ * @param {MicroAdType} microAdType
+ * @param {Device} device
+ * @returns {string | null}
+ */
+const getMicroAdUnitId = (index, microAdType, device = 'RWD') => {
+  let unitId = null
+
+  if (microAdType === 'LISTING') {
+    const unitIndex = Math.floor((index - 1) / 2)
+    unitId = MICRO_AD_UNITS.LISTING[device][unitIndex]?.id || null
+  } else if (microAdType === 'HOME') {
+    const unitIndex = Math.floor((index - 1) / 2)
+    unitId = MICRO_AD_UNITS.HOME[device][unitIndex]?.id || null
+  }
+
+  return unitId
+}
+
+export { needInsertMicroAdAfter, getMicroAdUnitId }
