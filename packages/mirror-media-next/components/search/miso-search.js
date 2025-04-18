6081030ee7718131a99340e88ab6bc30b83ca511
YiHsuan <v61265@gmail.com>
2025-04-18 17:38:07 +0800

    refactor(miso-search): adjust style of answer-box

--- packages/mirror-media-next/components/search/miso-search.js
+++ packages/mirror-media-next/components/search/miso-search.js
@@ -3,6 +3,200 @@ import { MISO_API_KEY } from '../../config/index.mjs'
 import styled from 'styled-components'
 
 const SearchWrapper = styled.div`
+  // input 框
+
+  // ask 區塊
+  .miso-hybrid-search-combo__answer {
+    padding: 0 22px !important;
+    border: 0;
+    ${({ theme }) => theme.breakpoint.md} {
+      max-width: 600px;
+      margin: 0 auto;
+      padding: 0 !important;
+    }
+    ${({ theme }) => theme.breakpoint.xl} {
+      max-width: 1024px;
+    }
+
+    .miso-hybrid-search-combo__answer-box {
+      border: 0;
+      box-shadow: none;
+
+      .miso-hybrid-search-combo__answer-box-inner {
+        max-height: inherit;
+        border: 0;
+      }
+
+      .miso-hybrid-search-combo__phrase {
+        font-size: 0;
+        margin-bottom: 20px;
+        miso-question {
+          display: block;
+          color: #1D9FB8;
+          text-align: center;
+          font-feature-settings: 'liga' off, 'clig' off;
+          font-family: "PingFang TC";
+          font-size: 28px;
+          font-style: normal;
+          font-weight: 600;
+          line-height: 115%; /* 32.2px */
+          letter-spacing: 0.5px;
+          padding-bottom: 32px;
+          border-bottom: 1px solid black;
+
+          &::before {
+            content: '你想找的是';
+            color: #1D9FB8;
+            text-align: center;
+            font-family: "PingFang TC";
+            font-size: 18px;
+            font-style: normal;
+            font-weight: 500;
+            line-height: 150%;
+            display: block;
+            margin-bottom: 27px;
+          }
+
+
+          ${({ theme }) => theme.breakpoint.md} {
+            display: flex;
+            border: 0;
+            font-size: 28px;
+            padding-bottom: 0;
+            align-items: center;
+            &:before {
+              margin-right: 28px;
+            }
+            &:after {
+              content: '';
+              flex: 1;
+              margin-left: 56px;
+              border-top: 1px solid black;
+              transform: translate(0, 50%)
+            }
+          }
+          ${({ theme }) => theme.breakpoint.xl} {
+            &:before {
+              margin-right: 36px;
+            }
+            &:after {
+              margin-left: 60px;
+            }
+          }
+        }
+      }
+
+      .miso-typewriter, .miso-typewriter * {
+        color: #000 !important;
+        font-family: "PingFang TC";
+        font-size: 18px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 200%; 
+        a::before {
+          color: #1D9FB8;
+        }
+      }
+
+      .miso-feedback {
+        display: none;
+      }
+    }
+
+    // source 文章
+    .miso-hybrid-search-combo__sources-container {
+      padding: 24px; 22px;
+      margin-top: 20px;
+      max-height: inherit;
+      border-top: 1px solid #000;
+      border-bottom: 1px solid #000;
+      position: relative;
+      z-index: 10;
+      ${({ theme }) => theme.breakpoint.md} {
+        margin-top: 24px;
+      }
+
+      h3 {
+        display: none;
+      }
+
+      .miso-list__list {
+        display: flex;
+        flex-direction: row;
+        overflow: scroll;
+        gap: 16px;
+        flex-wrap: nowrap;
+        ${({ theme }) => theme.breakpoint.md} {
+          gap: 48px;
+        }
+
+        .miso-list__item {
+          border: 0;
+          mark {
+            display: none;
+          }
+
+          .miso-list__item-body {
+            display: flex;
+            flex-direction: row-reverse;
+            gap: 8px !important;
+            ${({ theme }) => theme.breakpoint.md} {
+              gap: 10px !important;
+            }
+
+            .miso-list__item-cover-image-container {
+              width: 88px;
+              height: 60px !important;
+              margin-top: 32px;
+            }
+
+            .miso-list__item-index {
+              color: #1D9FB8;
+            }
+
+            .miso-list__item-info-container {
+              flex-direction: column-reverse;
+              flex: 1;
+              width: 139px;
+              margin-right: 4px;
+              ${({ theme }) => theme.breakpoint.md} {
+                margin-right: 2px;
+              }
+
+              .miso-list__item-date {
+                color: #9CB7C6;
+                font-family: "PingFang TC";
+                font-size: 14px;
+                font-style: normal;
+                font-weight: 400;
+                line-height: 14px;
+                margin-bottom: 8px;
+              }
+
+              .miso-list__item-title {
+                color: #000;
+                font-feature-settings: 'liga' off, 'clig' off;
+                font-family: "PingFang TC";
+                font-size: 18px;
+                font-style: normal;
+                font-weight: 400;
+                line-height: normal;
+                -webkit-line-clamp: 4;
+                padding: 0;
+                margin: 0;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    .miso-hybrid-search-combo__answer-box-toggle-container {
+      display: none;
+    }
+  }
+
+  // result 區域
   .miso-hybrid-search-combo__search-results {
     padding: 0 !important;
     ${({ theme }) => theme.breakpoint.md} {
