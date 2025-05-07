04e107886bb40b716f11cb8690a26986cfeba999
LinHeMa <linhema0921@gmail.com>
2025-05-07 15:02:09 +0800

    refactor: change props name

--- packages/mirror-media-next/components/story/shared/client-side-portal.js
+++ packages/mirror-media-next/components/story/shared/client-side-portal.js
@@ -3,22 +3,23 @@ import ReactDOM from 'react-dom'
 
 /**
  *
+ * reference: https://react.dev/reference/react-dom/createPortal
+ *
  * @param {Object} props
- * @param {boolean} props.isMobile - If it is on mobile, render through portal.
+ * @param {boolean} props.isTransport - If it is on mobile, render through portal.
  * @param {string} props.selector - The class name or ID to put the component.
  * @param {React.ReactNode} props.children - The children to render
  * @returns {React.ReactNode}
  */
-export default function ResponsivePortal({ isMobile, selector, children }) {
+export default function ResponsivePortal({ isTransport, selector, children }) {
   const [portalTarget, setPortalTarget] = useState(null)
 
-  // Make sure the document is mount.
   useEffect(() => {
     const target = document.querySelector(selector)
     setPortalTarget(target)
   }, [selector])
 
-  if (isMobile && portalTarget) {
+  if (isTransport && portalTarget) {
     return ReactDOM.createPortal(children, portalTarget)
   }
 
