173215f986354eeafaf0c1e98ad6769c4dcc3d06
YiHsuan <v61265@gmail.com>
2025-05-21 14:18:32 +0800

    chore: enable subscription pricing toggle in staging

--- packages/mirror-media-next/config/index.mjs
+++ packages/mirror-media-next/config/index.mjs
@@ -169,7 +169,7 @@ switch (ENV) {
 
     GCP_STACKDRIVER_LOG_NAME = 'mirror-media-next-user-behavior_staging'
     GCP_STACKDRIVER_ERROR_LOG_NAME = 'mirror-media-next-error-log_staging'
-    IS_PRIZE_RIZED = false
+    IS_PRIZE_RIZED = true
 
     COURSE_URL = 'https://course.mirrormedia.mg'
 
