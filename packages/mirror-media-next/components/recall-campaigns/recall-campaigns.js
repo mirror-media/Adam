d7dbd8d1bbdfe85232bf45129ec0996130a93a34
jyunhsin <ffworkbench@gmail.com>
2025-07-18 15:49:44 +0800

    chore: log environment variables for testing - 2

--- packages/mirror-media-next/components/recall-campaigns/recall-campaigns.js
+++ packages/mirror-media-next/components/recall-campaigns/recall-campaigns.js
@@ -55,19 +55,20 @@ const ViewMoreLink = styled.a`
 const prefix = ['prod', 'staging'].includes(ENV) ? 'www' : 'dev'
 const iframeSrc = `https://${prefix}.mirrormedia.mg/projects/election2025-homepage/index.html`
 
-const variables = process.env.NEXT_PUBLIC_SPECIALEVENT
-const newVariables = JSON.parse(
-  process.env.NEXT_PUBLIC_SPECIALEVENT.toLowerCase()
-)
+const variable = process.env.NEXT_PUBLIC_SPECIALEVENT
+const newVariable =
+  typeof variable === 'string'
+    ? JSON.parse(variable.toLowerCase())
+    : `${process.env.NEXT_PUBLIC_SPECIALEVENT} not string`
 
 export default function RecallCampaigns({ className = '' }) {
   console.log(
-    variables,
-    typeof variables,
+    variable,
+    typeof variable,
     'process.env.NEXT_PUBLIC_SPECIALEVENT - test'
   )
 
-  console.log(newVariables, typeof newVariables, 'newVariables - test')
+  console.log(newVariable, typeof newVariable, 'newVariable - test')
 
   if (!IS_SPECIAL_EVENT) return null
 
