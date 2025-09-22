06f9014c87eae7e562f1fe9100c263c3bb4d5d76
nihclil <oxx921@gmail.com>
2025-09-22 17:50:14 +0800

    style: adjust anniversary wrapper

--- packages/mirror-media-next/components/subscribe/anniversary-message.js
+++ packages/mirror-media-next/components/subscribe/anniversary-message.js
@@ -1,7 +1,5 @@
 import styled from 'styled-components'
 
-const Wrapper = styled.div``
-
 const Message = styled.p`
   color: #ea4335;
   font-weight: 500;
@@ -22,9 +20,9 @@ const Duration = styled.p`
 
 export default function AnniversaryMessage() {
   return (
-    <Wrapper>
+    <div>
       <Message>鏡週刊 10 週年慶 全站文章、獨家新聞全網免費閱讀</Message>
       <Duration>活動期間：2025/10/1 — 2025/12/31</Duration>
-    </Wrapper>
+    </div>
   )
 }
