eb8017c75ec98e96f0367f9f5dfa181152d5a60a
nihclil <oxx921@gmail.com>
2025-09-22 18:25:54 +0800

    styles: adjust /subscribe annivesary duration styles

--- packages/mirror-media-next/components/subscribe/anniversary-message.js
+++ packages/mirror-media-next/components/subscribe/anniversary-message.js
@@ -1,5 +1,11 @@
 import styled from 'styled-components'
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
 const Message = styled.p`
   color: #ea4335;
   font-weight: 500;
@@ -20,9 +26,9 @@ const Duration = styled.p`
 
 export default function AnniversaryMessage() {
   return (
-    <div>
+    <Wrapper>
       <Message>鏡週刊 10 週年慶 全站文章、獨家新聞全網免費閱讀</Message>
       <Duration>活動期間：2025/10/1 — 2025/12/31</Duration>
-    </div>
+    </Wrapper>
   )
 }
