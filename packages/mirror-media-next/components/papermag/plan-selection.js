b078c9bba82fc11107ae7aee90835c93bd73626c
Wendy Hu <yunhu412@gmail.com>
2025-10-09 14:59:46 +0800

    fix(papermag): replace mailto with a direct link to Gmail composer

--- packages/mirror-media-next/components/papermag/plan-selection.js
+++ packages/mirror-media-next/components/papermag/plan-selection.js
@@ -212,7 +212,11 @@ export default function PageBody() {
             ))}
             <PlanCTA>
               <CtaText>師生優惠方案、企業訂閱優惠</CtaText>
-              <Link href="mailto:service@mirrormedia.mg" target="_top">
+              <Link
+                href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&to=service@mirrormedia.mg"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
                 <CtaBtn>聯絡我們</CtaBtn>
               </Link>
             </PlanCTA>
