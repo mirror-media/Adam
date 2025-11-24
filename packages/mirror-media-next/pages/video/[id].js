da9b990729fcb07202528b07e9b11561e3c75f35
Wendy Hu <yunhu412@gmail.com>
2025-11-24 17:46:06 +0800

    style(video): wrap ads E1 & FT in GPT_Placeholder component

--- packages/mirror-media-next/pages/video/[id].js
+++ packages/mirror-media-next/pages/video/[id].js
@@ -66,30 +66,6 @@ const StyledGPTAd_HD = styled(GPTAd)`
   height: auto;
 `
 
-const StyledGPTAd_E1 = styled(GPTAd)`
-  width: 100%;
-  min-height: 250px;
-  margin: 20px auto 0px;
-
-  ${({ theme }) => theme.breakpoint.md} {
-    margin: 40px auto 0px;
-  }
-
-  ${({ theme }) => theme.breakpoint.xl} {
-    display: none;
-  }
-`
-
-const StyledGPTAd_FT = styled(GPTAd)`
-  width: 100%;
-  min-height: 250px;
-  margin: 20px auto 0px;
-
-  ${({ theme }) => theme.breakpoint.xl} {
-    margin: 28px auto 0px;
-  }
-`
-
 const StickyGPTAd = styled(GPTMbStAd)`
   position: fixed;
   left: 0;
@@ -105,10 +81,16 @@ const StickyGPTAd = styled(GPTMbStAd)`
   }
 `
 
-const GPT_PLACEHOLDER_SIZES = {
+const TOP_GPT_PLACEHOLDER_SIZES = {
   mobile: { width: '300px', height: '350px', margin: '8px auto' },
   tablet: { width: '300px', height: '350px', margin: '24px auto' },
   desktop: { width: '970px', height: '250px', margin: '0px auto 28px' },
+}
+
+const BOTTOM_GPT_PLACEHOLDER_SIZES = {
+  mobile: { width: '300px', height: '250px', margin: '20px auto 0px' },
+  tablet: { width: '300px', height: '250px', margin: '40px auto 0px' },
+  desktop: { width: '970px', height: '250px', margin: '28px auto 0px' },
 }
 
 /**
@@ -140,7 +122,7 @@ export default function Video({ video, latestVideos, headerData }) {
         <Wrapper>
           <GPT_Placeholder
             displayAt="desktop"
-            rwd={GPT_PLACEHOLDER_SIZES}
+            rwd={TOP_GPT_PLACEHOLDER_SIZES}
             shouldShowAd={shouldShowAd}
             isLogInProcessFinished={isLogInProcessFinished}
           >
@@ -152,7 +134,7 @@ export default function Video({ video, latestVideos, headerData }) {
 
           <GPT_Placeholder
             displayAt="mobile+tablet"
-            rwd={GPT_PLACEHOLDER_SIZES}
+            rwd={TOP_GPT_PLACEHOLDER_SIZES}
             shouldShowAd={shouldShowAd}
             isLogInProcessFinished={isLogInProcessFinished}
           >
@@ -163,7 +145,14 @@ export default function Video({ video, latestVideos, headerData }) {
 
           <ContentWrapper>
             <YoutubeArticle video={video} />
-            {shouldShowAd && <StyledGPTAd_E1 pageKey="videohub" adKey="E1" />}
+            <GPT_Placeholder
+              displayAt="mobile+tablet"
+              rwd={BOTTOM_GPT_PLACEHOLDER_SIZES}
+              shouldShowAd={shouldShowAd}
+              isLogInProcessFinished={isLogInProcessFinished}
+            >
+              {shouldShowAd && <StyledGPTAd_HD pageKey="videohub" adKey="E1" />}
+            </GPT_Placeholder>
             {latestVideos.length > 0 && (
               <VideoList
                 videos={latestVideos}
@@ -174,21 +163,17 @@ export default function Video({ video, latestVideos, headerData }) {
 
           <YoutubePolicy />
 
-          {shouldShowAd && (
-            <>
-              <GPT_Placeholder
-                displayAt="all"
-                rwd={GPT_PLACEHOLDER_SIZES}
-                shouldShowAd={shouldShowAd}
-                isLogInProcessFinished={isLogInProcessFinished}
-              >
-                <StyledGPTAd_FT pageKey="videohub" adKey="FT" />
-              </GPT_Placeholder>
+          <GPT_Placeholder
+            displayAt="all"
+            rwd={BOTTOM_GPT_PLACEHOLDER_SIZES}
+            shouldShowAd={shouldShowAd}
+            isLogInProcessFinished={isLogInProcessFinished}
+          >
+            {shouldShowAd && <StyledGPTAd_HD pageKey="videohub" adKey="FT" />}
+          </GPT_Placeholder>
 
-              <StickyGPTAd pageKey="videohub" />
-              {hasScrolled && <FullScreenAds />}
-            </>
-          )}
+          <StickyGPTAd pageKey="videohub" />
+          {hasScrolled && <FullScreenAds />}
         </Wrapper>
       </Layout>
     </>
