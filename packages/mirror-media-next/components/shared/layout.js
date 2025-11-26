3fca173a5547a8acea1c41e6a001dffd51d9ac2a
Wendy Hu <yunhu412@gmail.com>
2025-11-26 17:59:26 +0800

    feat(layout): pass ogDescription from Layout to CustomHead

--- packages/mirror-media-next/components/shared/layout.js
+++ packages/mirror-media-next/components/shared/layout.js
@@ -31,6 +31,7 @@ export default function Layout({ head, header, footer, children }) {
         title={head?.title}
         ogTitle={head?.ogTitle}
         description={head?.description}
+        ogDescription={head?.ogDescription}
         imageUrl={head?.imageUrl}
         skipCanonical={head?.skipCanonical}
         pageType={head?.pageType}
