18acadd5b7ad68137b8975d42c6d35a632f1e7bf
Wendy Hu <yunhu412@gmail.com>
2026-03-12 17:33:27 +0800

    feat(api): add fetchNewsCategoryPostsJSON

--- packages/mirror-media-next/utils/api/category.js
+++ packages/mirror-media-next/utils/api/category.js
@@ -1,6 +1,11 @@
 import client from '../../apollo/apollo-client'
 import { fetchCategorySections } from '../../apollo/query/categroies'
 import { fetchPosts } from '../../apollo/query/posts'
+import axios from 'axios'
+import {
+  API_TIMEOUT,
+  URL_STATIC_NEWS_CATEGORY_POSTS,
+} from '../../config/index.mjs'
 
 export function fetchPostsByCategorySlug(categorySlug, take, skip) {
   return client.query({
@@ -15,6 +20,42 @@ export function fetchPostsByCategorySlug(categorySlug, take, skip) {
       },
     },
   })
+}
+
+export async function fetchNewsCategoryPostsJSON(page = 1, take = 24) {
+  const POSTS_PER_JSON = 120
+  const TAKE_PER_JSON = POSTS_PER_JSON / take
+  const jsonFileOrder = Math.ceil(page / TAKE_PER_JSON)
+  const jsonUrl = `${URL_STATIC_NEWS_CATEGORY_POSTS}_${jsonFileOrder}.json`
+
+  try {
+    const response = await axios({
+      method: 'get',
+      url: jsonUrl,
+      timeout: API_TIMEOUT,
+    })
+
+    const jsonIndex = (page - 1) % TAKE_PER_JSON
+    const startIndex = jsonIndex * take
+    const endIndex = startIndex + take
+    const postItems = response.data.posts.items.slice(startIndex, endIndex)
+
+    return {
+      data: {
+        posts: {
+          items: jsonFileOrder <= 4 ? postItems : [],
+          counts:
+            response.data.posts.counts.posts +
+            response.data.posts.counts.externals,
+        },
+      },
+    }
+  } catch (err) {
+    console.error(
+      'Failed to fetch JSON of URL_STATIC_NEWS_CATEGORY_POSTS',
+      JSON.stringify(err)
+    )
+  }
 }
 
 export function fetchPremiumPostsByCategorySlug(categorySlug, take, skip) {
