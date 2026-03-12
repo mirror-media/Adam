945111f5c3e03d241d9e5fd174d986ec88231d4d
Wendy Hu <yunhu412@gmail.com>
2026-03-12 17:49:13 +0800

    feat(config): add URL_STATIC_NEWS_CATEGORY_INFO

--- packages/mirror-media-next/config/index.mjs
+++ packages/mirror-media-next/config/index.mjs
@@ -59,6 +59,7 @@ let STORY_GQL_ENDPOINT = ''
 let ENABLE_NON_PREMIUM_OPEN_ARTICLE_MODE = true
 let GCS_FUSE_MOUNT_DIR = ''
 let GCS_FUSE_STATIC_BUCKET = ''
+let URL_STATIC_NEWS_CATEGORY_INFO = ''
 let URL_STATIC_NEWS_CATEGORY_POSTS = ''
 /** @type {import("firebase/auth").ActionCodeSettings} */
 let ACTION_CODE_SETTING
@@ -97,6 +98,7 @@ switch (ENV) {
     URL_STATIC_PROMOTE_VIDEOS = `https://${STATIC_FILE_DOMAIN}/files/json/promoting-video.json`
     URL_STATIC_COLUMN_SECTION_POSTS = `https://${STATIC_FILE_DOMAIN}/json/atest/latest_content_section_column_1`
     URL_STATIC_DAILY_COLUMN_HEADLINES = `https://${STATIC_FILE_DOMAIN}/files/json/daily-column.json`
+    URL_STATIC_NEWS_CATEGORY_INFO = `https://${STATIC_FILE_DOMAIN}/json/latest/category_news.json`
     URL_STATIC_NEWS_CATEGORY_POSTS = `https://${STATIC_FILE_DOMAIN}/json/latest/latest_content_category_news_public`
     // Only use STORY_GQL_ENDPOINT if explicitly set via env var
     // Do not fallback to dev endpoint in prod to avoid connecting to dev service
@@ -166,6 +168,7 @@ switch (ENV) {
     URL_STATIC_PROMOTE_VIDEOS = `https://${STATIC_FILE_DOMAIN}/files/json/promoting-video.json`
     URL_STATIC_COLUMN_SECTION_POSTS = `https://${STATIC_FILE_DOMAIN}/json/atest/latest_content_section_column_1`
     URL_STATIC_DAILY_COLUMN_HEADLINES = `https://${STATIC_FILE_DOMAIN}/files/json/daily-column.json`
+    URL_STATIC_NEWS_CATEGORY_INFO = `https://${STATIC_FILE_DOMAIN}/json/latest/category_news.json`
     URL_STATIC_NEWS_CATEGORY_POSTS = `https://${STATIC_FILE_DOMAIN}/json/latest/latest_content_category_news_public`
     STORY_GQL_ENDPOINT =
       process.env.STORY_GQL_ENDPOINT ||
@@ -232,6 +235,7 @@ switch (ENV) {
     URL_STATIC_PROMOTE_VIDEOS = `https://${STATIC_FILE_DOMAIN}/files/json/promoting-video.json`
     URL_STATIC_COLUMN_SECTION_POSTS = `https://storage.googleapis.com/v3-statics-dev.mirrormedia.mg/json/latest/latest_content_section_column_1.json`
     URL_STATIC_DAILY_COLUMN_HEADLINES = `https://${STATIC_FILE_DOMAIN}/files/json/daily-column.json`
+    URL_STATIC_NEWS_CATEGORY_INFO = `https://${STATIC_FILE_DOMAIN}/json/latest/category_news.json`
     URL_STATIC_NEWS_CATEGORY_POSTS = `https://${STATIC_FILE_DOMAIN}/json/latest/latest_content_category_news_public`
     STORY_GQL_ENDPOINT =
       process.env.STORY_GQL_ENDPOINT ||
@@ -304,6 +308,7 @@ switch (ENV) {
     URL_STATIC_PROMOTE_VIDEOS = `https://${STATIC_FILE_DOMAIN}/files/json/promoting-video.json`
     URL_STATIC_COLUMN_SECTION_POSTS = `https://storage.googleapis.com/v3-statics-dev.mirrormedia.mg/json/latest/latest_content_section_column_1.json`
     URL_STATIC_DAILY_COLUMN_HEADLINES = `https://${STATIC_FILE_DOMAIN}/files/json/daily-column.json`
+    URL_STATIC_NEWS_CATEGORY_INFO = `https://${STATIC_FILE_DOMAIN}/json/latest/category_news.json`
     URL_STATIC_NEWS_CATEGORY_POSTS = `https://${STATIC_FILE_DOMAIN}/json/latest/latest_content_category_news_public`
     STORY_GQL_ENDPOINT =
       process.env.STORY_GQL_ENDPOINT ||
@@ -389,5 +394,6 @@ export {
   ENABLE_NON_PREMIUM_OPEN_ARTICLE_MODE,
   GCS_FUSE_MOUNT_DIR,
   GCS_FUSE_STATIC_BUCKET,
+  URL_STATIC_NEWS_CATEGORY_INFO,
   URL_STATIC_NEWS_CATEGORY_POSTS,
 }
