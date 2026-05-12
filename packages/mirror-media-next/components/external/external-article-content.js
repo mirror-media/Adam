8bda93535a5b04cec60c499a6c9cf2688f9bb6c5
Wendy Hu <yunhu412@gmail.com>
2026-05-12 18:45:55 +0800

    feat(external): implement arrow icon for reading first related article

--- packages/mirror-media-next/components/external/external-article-content.js
+++ packages/mirror-media-next/components/external/external-article-content.js
@@ -2,6 +2,11 @@ import styled from 'styled-components'
 import MirrorMedia from '@mirrormedia/lilith-draft-renderer/lib/website/mirrormedia'
 import ExternalEmbedCodeBlock from './external-embed-code-block'
 const { draftEditorCssExternal } = MirrorMedia
+import Link from 'next/link'
+import Image from 'next/image'
+import { Z_INDEX } from '../../constants/index'
+
+const leftArrow = '/images-next/story/left-arrow.svg'
 
 const Wrapper = styled.section`
   margin-top: 32px;
@@ -34,16 +39,31 @@ const Wrapper = styled.section`
     overflow: hidden;
   }
 `
-
+const ImageWrapper = styled.div`
+  padding: 16px;
+  position: fixed;
+  bottom: 25%;
+  right: 0;
+  z-index: ${Z_INDEX.storyLeftArrow};
+`
 /**
  *
  * @param {Object} props
  * @param {string} props.content
+ * @param {Array} props.allRelatedStories
+ * @param {boolean} props.isMobileWidth
  * @returns {import('react').JSX.Element}
  */
-export default function ExternalArticleContent({ content = '' }) {
+export default function ExternalArticleContent({
+  content = '',
+  allRelatedStories = [],
+  isMobileWidth,
+}) {
   const iframeRegex = /(<iframe[\s\S]*?<\/iframe>)/i
   const parts = content.split(iframeRegex).filter((p) => p.trim())
+  const hasFirstRelatedArticle = allRelatedStories.length
+    ? allRelatedStories[0]
+    : null
   return (
     <Wrapper>
       {parts.map((part, index) => {
@@ -52,6 +72,29 @@ export default function ExternalArticleContent({ content = '' }) {
         }
         return <div dangerouslySetInnerHTML={{ __html: part }} key={index} />
       })}
+
+      {isMobileWidth && hasFirstRelatedArticle && (
+        <Link
+          href={
+            allRelatedStories[0].url ??
+            `${
+              allRelatedStories[0].__typename === 'Post'
+                ? '/story'
+                : '/external'
+            }/${allRelatedStories[0].slug}`
+          }
+          target="_blank"
+        >
+          <ImageWrapper>
+            <Image
+              src={leftArrow}
+              width={24}
+              height={24}
+              alt="點按看下一則延伸閱讀"
+            />
+          </ImageWrapper>
+        </Link>
+      )}
     </Wrapper>
   )
 }
